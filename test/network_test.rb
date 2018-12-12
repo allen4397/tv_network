f34012b9a5b68b31a0c348c46b726c497139e6c0
Tim Allen <tnelsallen3@gmail.com>
2018-12-12 09:46:58 -0700

    Fixes add show test by adding nbc network

--- test/network_test.rb
+++ test/network_test.rb
@@ -19,6 +19,7 @@ class NetworkTest < Minitest::Test
   end
 
   def test_it_can_add_shows
+    nbc = Network.new("NBC")
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
