46f2f813caa3050bedd8a16ff1a2c27234e6c729
Tim Allen <tnelsallen3@gmail.com>
2018-12-12 10:08:24 -0700

    Adds highest paid actor method

--- lib/network.rb
+++ lib/network.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Network
   attr_reader :name,
               :shows
@@ -9,5 +11,16 @@ class Network
 
   def add_show(show)
     @shows << show
+  end
+
+  def highest_paid_actor
+    highest_salary_character = nil
+    shows.max_by do |show|
+      highest_salary_character = show.characters.max_by do |character|
+        character.salary
+      end
+      highest_salary_character.salary
+    end
+    highest_salary_character.actor
   end
 end
